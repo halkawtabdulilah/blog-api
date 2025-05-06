9bfea7e7f06e78964fd27bb76c08ea5145866b55
Halkawt-Abdulilah <halkawt.abdulilah@gmail.com>
2025-05-06 21:30:51 +0300

    feat(posts): create endpoint with validation

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -2,9 +2,27 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Category;
 use Illuminate\Http\Request;
 
 class CategoryController extends Controller
 {
-    //
+    public function index(Request $request) {}
+
+
+    public function store(Request $request) {
+        $validated = $request->validate([
+            "name" => "required|string|max:255|unique:categories,name",
+            "slug" => "required|string|max:255|unique:categories,slug",
+            "description" => "nullable|string|max:255",
+        ]);
+
+        $category = Category::create($validated);
+
+        return response()->json($category, 201);
+
+    }
+    public function show(Request $request) {}
+    public function update(Request $request) {}
+    public function delete(Request $request) {}
 }
---
+++ tests/Feature/CreateCategoryTest.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Tests\Feature;
+
+// use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Tests\TestCase;
+
+class CreateCategoryTest extends TestCase
+{
+
+    use RefreshDatabase;
+
+    /**
+     * A basic test example.
+     */
+    public function test_user_can_create_category(): void
+    {
+
+        $userData = [
+            "name" => "Test Category",
+            "slug" => "test-category",
+            "description" => "Test Category description",
+        ];
+
+        $response = $this->postJson('/api/category', $userData);
+
+        $response->assertStatus(201);
+    }
+}
